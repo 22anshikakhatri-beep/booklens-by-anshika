bc462ca2751a94bdc8edf3b250eb805e785d9155
22anshikakhatri-beep <22anshika.khatri@gmail.com>
2025-09-14 21:48:54 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -135,7 +135,7 @@ export default function Home() {
 
           {/* CHANGE #3: Position mascot (green box) and bubble (red box) */}
           {/* We make this area the anchor for absolute positioning */}
-          <div className="relative mx-auto max-w-[980px] h-[180px] mt-9">
+          <div className="relative mx-auto max-w-[980px] h-[180px] mt-8">
             {/* Mascot at the left area (green box location) */}
             <div className="absolute left-[90px] md:left-[120px] lg:left-[140px] top-[10px] md:top-[8px]">
               {/* eslint-disable-next-line @next/next/no-img-element */}
